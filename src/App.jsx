dee033e15dd07be60aa11b74706980c0d02b32c5
MohamedELBERMIL69780 <mohamed-du-69780@hotmail.fr>
2025-05-06 10:27:31 +0200

    css de la navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import './App.css'
-import {Navbar} from "./components/Navbar/Navbar"
+import "./App.css";
+import "./components/Navbar/Navbar.module.css";
+import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
-
   return (
     <>
       <Navbar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import "./Navbar.module.css";
 
 export const Navbar = () => {
-    return(
-        <>
-        <h1>test</h1>
-        </>
-    )
-}
+  return (
+    <>
+      <nav>
+        <div className="status">
+          <span>Mohamed EL BERMIL</span>
+          <div className="btn-status">
+            <div className="btn">Permis</div>
+            <div className="btn">
+              Recherche d’alterance à partir du 12/08/2025
+            </div>
+          </div>
+        </div>
+        <ul>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Work</a>
+          </li>
+          <li>
+            {/* arrow */}
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};

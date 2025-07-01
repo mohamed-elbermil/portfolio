0db727708b4f5c09cbc0be24f0decd31a362ffc7
MohamedELBERMIL69780 <mohamed-du-69780@hotmail.fr>
2025-07-01 10:28:30 +0200

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
 import Navbar from '../Navbar/Navbar'
-
+import styles from './Header.module.css'
 function Header() {
   return (
     <>
       <Navbar />
       <div>
-        <h1>Hello World</h1>
+        <div className={styles.status}>
+            <div className={styles.roundStatus}></div>
+            <p>Disponible</p>
+        </div>
+        <h1>Hello,zgertyzerrtjykui</h1>
+        <p>zerthjergthyjzerthjergthyjzerthjergthyjzerthjergthyjzerthjergthyjzerthjergthyjzerthjergthyjzerthjergthyj</p>
+        <a href="#contact" id='cv'>Curriculum vitae</a>
       </div>
     </>
   )

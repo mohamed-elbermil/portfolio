53e722c350665a487ed6b737fb9c1ac5474144e8
MohamedELBERMIL69780 <mohamed-du-69780@hotmail.fr>
2025-07-04 11:03:13 +0200

    MAJ header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,33 +6,44 @@ function Header() {
     <>
       <Navbar />
       <div className={styles.header}>
-
-      <div>
+      <div className={styles['left-side-header']}>
         <div className={styles.status}>
             <div className={styles.roundStatus}></div>
             <p>Disponible</p>
         </div>
-        <h1 className={styles.h1}>Hello,zgertyzerrtjykui</h1>
+        <h1 className={styles.h1}>Hello,zgertyze <br /> rrtjykui</h1>
         <p>zerthjergthyjzerthjergthyjzerthjergthyjzerthjergthyjzerthjergthyjzerthjergthyjzerthjergthyjzerthjergthyj</p>
-        <a href="#contact" id='cv'>Curriculum vitae</a>
+        <a href="cv-mohamed-elbermil.pdf" download className={styles.cv}>
+          <i className="fa-solid fa-download"></i>
+          Curriculum vitae
+        </a>
       </div>
       <div className={styles['right-side-header']}>
-        <div className={styles['fitness-area-cover']}>
-            <div className={styles.title}>
-                <p>Fitness-Area</p>
-                <span>Salle de sport</span>
-                <div className={styles.time}>
-                    
-                </div>
-            </div>
-            <div className={styles.both}>
-                <span className={styles.ticket}>1 week</span>
-                <a href="#" className={styles.ticket}>Accéder au site</a>
-            </div>
+        <div className={styles['bloc-top']}>
+          <div className={styles['fitness-area-cover']}>
+              <div className={styles.title}>
+                  <p>Fitness-Area</p>
+                  <span>Salle de sport</span>
+                  <div className={styles.time}>
+                      
+                  </div>
+              </div>
+              <div className={styles.both}>
+                  <span className={styles.ticket}>1 week</span>
+                  <a href="#" className={styles.ticket}>Accéder au site</a>
+              </div>
+          </div>
+          <img src={Profile} alt="Photo de profil" />
         </div>
-        <img src={Profile} alt="Photo de profil" />
-        <div className={styles['social-media-round']}>
-            <div className={styles['icon-social-media']}></div>
+        <div className={styles['bloc-bottom']}>
+          <div className={styles['bloc-left']}>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis sunt recusandae nihil!</p>
+          </div>
+          <div className={styles['bloc-right']}>
+            <a href="https://www.linkedin.com/in/mohamed-el-bermil-b957861bb/" target='blank'><i class="fa-brands fa-linkedin-in"></i></a>
+            <a href="https://www.tiktok.com/@melbi6000" target='blank'><i class="fa-brands fa-tiktok"></i></a>
+            <a href="https://www.youtube.com/@melbi-099" target='blank'><i class="fa-brands fa-youtube"></i></a>
+          </div>
         </div>
       </div>
 

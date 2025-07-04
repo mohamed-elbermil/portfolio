a1d89c9cb0c0421233eb9530f84073a687929802
mohamed-elbermil <mohamed-du-69780@hotmail.fr>
2025-07-04 19:53:05 +0100

    ajout d'animation gsap

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,70 @@
 // Header.jsx
+import { useEffect, useRef } from 'react';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
 import Navbar from '../Navbar/Navbar';
 import styles from './Header.module.css';
 import Profile from '../../assets/images/profile.png';
 
+gsap.registerPlugin(ScrollTrigger);
+
 function Header() {
+  const headerRef = useRef(null);
+  const leftRef = useRef(null);
+  const rightRef = useRef(null);
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.fromTo(
+        leftRef.current.children,
+        { opacity: 0, y: 50 },
+        {
+          opacity: 1,
+          y: 0,
+          stagger: 0.2,
+          duration: 1,
+          ease: 'power3.out',
+          scrollTrigger: {
+            trigger: leftRef.current,
+            start: 'top center',   // animation démarre quand top de l’élément touche le centre de la fenêtre
+            end: 'bottom center',  // animation se termine quand bottom touche centre
+            toggleActions: 'play reverse play reverse', 
+            // play à l’entrée, reverse à la sortie, play à retour, reverse à sortie retour
+            markers: false,
+          },
+        }
+      );
+  
+      gsap.fromTo(
+        rightRef.current,
+        { opacity: 0, x: 100 },
+        {
+          opacity: 1,
+          x: 0,
+          duration: 1.2,
+          ease: 'power3.out',
+          scrollTrigger: {
+            trigger: rightRef.current,
+            start: 'top center',
+            end: 'bottom center',
+            toggleActions: 'play reverse play reverse',
+            markers: false,
+          },
+        }
+      );
+    }, headerRef);
+  
+    return () => ctx.revert();
+  }, []);
+  
+  
+
   return (
     <>
       <Navbar />
-      <header className={styles.header}>
-        <div className={styles.left}>
+      <header className={styles.header} ref={headerRef}>
+        <div className={styles.left} ref={leftRef}>
           <span className={styles.status}>
             <span className={styles.dot}></span>
             Disponible
@@ -27,12 +83,12 @@ function Header() {
           </div>
         </div>
 
-        <div className={styles.right}>
+        <div className={styles.right} ref={rightRef}>
           <div className={styles.card}>
             <img src={Profile} alt="Moi" className={styles.profile} />
             <div className={styles.badge}>
               <p>Mohamed EL BERMIL</p>
-              <span>Fondateur de l'agence <a href="https://soblim.com/" target="_blank">SOBLIM</a></span>
+              <span>Fondateur de l'agence <a href="https://soblim.com/" target="_blank" rel="noopener noreferrer">SOBLIM</a></span>
             </div>
           </div>
         </div>
